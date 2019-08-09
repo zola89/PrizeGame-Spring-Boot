88d038371b59872bcb75c245722134ee8a0277e5
zola89 <zola89@gmail.com>
2019-08-09 02:36:31 +0200

    added additional handlers

---
+++ prize-game-serverless/src/main/java/com/serverless/DeleteUserHandler.java
@@ -0,0 +1,54 @@
+package com.serverless;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.log4j.Logger;
+import java.util.Collections;
+import java.util.Map;
+
+import com.serverless.dal.User;
+
+public class DeleteUserHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+
+	private final Logger logger = Logger.getLogger(this.getClass());
+
+	@Override
+	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+
+    try {
+        // get the 'pathParameters' from input
+        Map<String,String> pathParameters =  (Map<String,String>)input.get("pathParameters");
+        String userId = pathParameters.get("id");
+
+        // get the User by id
+        Boolean success = new User().delete(userId);
+
+        // send the response back
+        if (success) {
+          return ApiGatewayResponse.builder()
+      				.setStatusCode(204)
+      				.setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+      				.build();
+        } else {
+          return ApiGatewayResponse.builder()
+      				.setStatusCode(404)
+      				.setObjectBody("User with id: '" + userId + "' not found.")
+      				.setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+      				.build();
+        }
+    } catch (Exception ex) {
+        logger.error("Error in deleting user: " + ex);
+
+        // send the error response back
+  			Response responseBody = new Response("Error in deleting user: ", input);
+  			return ApiGatewayResponse.builder()
+  					.setStatusCode(500)
+  					.setObjectBody(responseBody)
+  					.setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+  					.build();
+    }
+	}
+}
---
+++ prize-game-serverless/src/main/java/com/serverless/ListUsersHandler.java
@@ -0,0 +1,43 @@
+package com.serverless;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.log4j.Logger;
+import java.util.Collections;
+import java.util.Map;
+import java.util.List;
+
+import com.serverless.dal.User;
+
+public class ListUsersHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+
+	private final Logger logger = Logger.getLogger(this.getClass());
+
+	@Override
+	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+    try {
+        // get all users
+        List<User> users = new User().list();
+
+        // send the response back
+        return ApiGatewayResponse.builder()
+    				.setStatusCode(200)
+    				.setObjectBody(users)
+    				.setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+    				.build();
+    } catch (Exception ex) {
+        logger.error("Error in listing users: " + ex);
+
+        // send the error response back
+  			Response responseBody = new Response("Error in listing users: ", input);
+  			return ApiGatewayResponse.builder()
+  					.setStatusCode(500)
+  					.setObjectBody(responseBody)
+  					.setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+  					.build();
+    }
+	}
+}
